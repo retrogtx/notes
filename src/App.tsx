3726cae44d7317e7cb77ff31c960deaaf920911a
Amrit Rai <iamamrit27@gmail.com>
2024-03-11 18:54:40 +0530

    Refactor code to improve performance and readability

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="w-full h-screen bg-zinc-800">
+      <h1 className="text-[13vw] leading-none tracking-tigher">Docs</h1>
+    </div>
+  )
+}
+
+export default App;
