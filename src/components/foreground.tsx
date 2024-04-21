8afb97a3885c39ad6b978fea995ed61e66970fc5
Amrit Rai <iamamrit27@gmail.com>
2024-04-21 20:37:53 +0530

    modify card, add {} components

--- src/components/foreground.tsx
+++ src/components/foreground.tsx
@@ -2,9 +2,19 @@ import React from 'react'
 import Card from './card'
 
 function foreground() {
+  const data = [
+    {
+      desc: "Hi this is amrit",
+      size: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+    },
+  ]
   return (
     <div className="fixed top-0 left-0 z-[3] w-full h-full">
-        <Card />
+      {data.map((item, index) => (
+        <Card data={item} />
+      ))}
     </div>
   )
 }
