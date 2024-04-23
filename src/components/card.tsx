84b7695034b5475b48280801e5dbf4ee5eba4d71
Amrit Rai <iamamrit27@gmail.com>
2024-04-23 20:40:38 +0530

    Refactor card component and add IoClose icon

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from 'react-icons/lu';
+import { IoClose } from "react-icons/io5";
 
-function Card({ data }) {
+function Card({ data }: { data: any }) {
   return (
     <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
       <FaRegFileAlt />
@@ -11,12 +12,17 @@ function Card({ data }) {
         <div className='flex items-center justify-between px-8 py-3 mb-3'>
           <h5>{data.filesize}</h5>
           <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
-            <LuDownload size='.7em' color='#fff' />
+            {data.close ? <>{IoClose}</> : <>{<LuDownload size='.7em' color='#fff' />}</>}
           </span>
         </div>
-        <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-          <h3 className='text-sm font-semibold'>Download Now</h3>
-        </div>
+        {
+          data.tag.isOpen && (
+            <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
+              <h3 className='text-sm font-semibold'>Download Now</h3>
+            </div>
+          )
+        }
+
       </div>
     </div>
 
