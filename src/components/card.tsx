ecc87b59c2b28b3e9fea52ab8361cb80941f62d3
Amrit Rai <iamamrit27@gmail.com>
2024-04-26 18:45:09 +0530

    Final Refactor card component, add framer-motion animation, and update dependencies

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import { FaRegFileAlt } from "react-icons/fa";
 import { LuDownload } from 'react-icons/lu';
 import { IoClose } from "react-icons/io5";
+import { motion } from 'framer-motion';
 
-function Card({ data }: { data: any }) {
+function Card({ data, reference }: { data: any }) {
   return (
-    <div className='relative w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden'>
+    <motion.div drag dragConstraints={reference} className='relative flex-shrink-0 w-60 h-72 rounded-[40px] bg-zinc-900/90 text-white px-8 py-10 overflow-hidden '>
       <FaRegFileAlt />
       <p className='text-sm leading-tight mt-5 font-semibold'>{data.desc}</p>
       <div className='footer absolute bottom-0 w-full left-0'>
@@ -14,17 +15,17 @@ function Card({ data }: { data: any }) {
           <span className='w-7 h-7 bg-zinc-600 rounded-full flex items-center justify-center'>
             {data.close ? <>{IoClose}</> : <>{<LuDownload size='.7em' color='#fff' />}</>}
           </span>
-        </div>
+        </div>s
         {
           data.tag.isOpen && (
-            <div className='tag w-full py-4 bg-green-600 flex items-center justify-center'>
-              <h3 className='text-sm font-semibold'>Download Now</h3>
+            <div className={`tag w-full py-4 bg-${data.tag.tagColor}-600 flex items-center justify-center`}>
+              <h3 className='text-sm font-semibold'>{data.tag.tagTitle}</h3>
             </div>
           )
         }
 
       </div>
-    </div>
+    </motion.div>
 
   )
 }
--- src/components/foreground.tsx
+++ src/components/foreground.tsx
@@ -2,7 +2,20 @@ import React from 'react'
 import Card from './card'
 
 function foreground() {
+  const ref = React.useRef(null);
   const data = [
+    {
+      desc: "Hi this is amrit",
+      size: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+    },
+    {
+      desc: "Hi this is amrit",
+      size: ".9mb",
+      close: true,
+      tag: { isOpen: true, tagTitle: "Download Now", tagColor: "green" }
+    },
     {
       desc: "Hi this is amrit",
       size: ".9mb",
@@ -11,9 +24,9 @@ function foreground() {
     },
   ]
   return (
-    <div className="fixed top-0 left-0 z-[3] w-full h-full">
+    <div ref={ref} className="fixed p-5 top-0 left-0 z-[3] w-full h-full flex gap-5 flex-wrap">
       {data.map((item, index) => (
-        <Card data={item} />
+        <Card data={item} reference={ref} />
       ))}
     </div>
   )
